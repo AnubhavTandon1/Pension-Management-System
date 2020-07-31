9e67e768684a93a929cb163c136d08c089aaf15d
Anubhav <anubhavtandon@cognizant.com>
2020-07-31 14:25:13 +0530

    First commit

---
+++ process-pension-module/src/main/java/com/cognizant/processpensionmodule/ProcessPensionModuleApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.processpensionmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessPensionModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessPensionModuleApplication.class, args);
+	}
+
+}
