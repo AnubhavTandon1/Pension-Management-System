2dfc5d4a503116fdee01e46734b4a026ce8f550b
Brijesht-848556 <69032401+Brijesht-848556@users.noreply.github.com>
2020-08-01 18:45:52 +0530

    Add files via upload
    1. Created login.jsp for web portal 
    2. created a controller for web portal

---
+++ webportal/src/main/java/loginController/login.java
@@ -0,0 +1,35 @@
+package loginController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class login {
+	@Autowired
+	  UserService userService;
+	
+	  
+	  @RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
+	  public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+	  @ModelAttribute("login") Login login) {
+	    ModelAndView mav = null;
+	    User user = userService.validateUser(login);
+	    if (null != user) {
+	    mav = new ModelAndView("welcome");
+	    mav.addObject("firstname", user.getFirstname());
+	    } else {
+	    mav = new ModelAndView("login");
+	    mav.addObject("message", "Username or Password is wrong!!");
+	    }
+	    return mav;
+	  }
+
+}
